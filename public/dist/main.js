33004d1f4f7bd44b96e90334ef889a7027be5c2c
Yasmine091 <yasmine.emdounan@gmail.com>
2020-12-01 12:37:50 +0100

    CI + Image maquette

---
+++ public/dist/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function helloWorld() {
+  console.log('Hello');
+}
+
+helloWorld();
+
+var helloWorldEs6 = function helloWorldEs6() {
+  console.log('Hello new world');
+};
+
+helloWorldEs6();
